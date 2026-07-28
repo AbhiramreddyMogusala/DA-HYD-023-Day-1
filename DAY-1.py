c04201f17a834156a2a4a089b11db2f2089c25d8
Abhiram reddy <abhiramreddymogusala@gmail.com>
2026-07-28 15:02:06 +0530

    day-1

---
+++ DAY-1.py
@@ -0,0 +1,50 @@
+#print('Hello world')
+#IDLE is color coding environment
+#hasgtag is used as single line comment
+#we use triple quotes(single/double) '''',"" as multiline comments
+
+'''
+tokens-->a smallest unit in a program.There are five types of tokens.
+->Keywords
+->Identifiers
+->Literals
+->Operators
+->Punctuators
+'''
+#Keywords  -->These arew reserved words in python.We have 35 keywords
+#help() --> it gives list of 35 keywords.
+#identifiers -->These are the names being alloted to vbariables,functions,class...
+#We cannot start with a number.
+#We can start with uppercase/lowercase letters(A-Z) but not special characters like #,@,$,*
+#No space between letters, where as we can also use underscore as an identifier_
+#Python is acase sensitive language.
+'''
+name='Abhiram'
+print(name)
+email_id='anhiramreddymogusala@gmail.com'
+#print(Email_id) #python is Case-sensitive -->NameError
+#1swer=23
+#print(1swer)
+#@defrrrr=3.4  #No special characters.
+
+#True=45 #No keywords
+
+batch="da23"
+print(batch)
+
+#Literals -->These are constants (value(s)) -->number,character
+
+#Operators -->Operators perform specific operation -->7types
+
+length=5
+breadth=3
+area=length*breadth
+print(area)
+
+#Multi assignments of variables
+name='Abhioram'
+'''
+name,location,age='Abhiram','HYd',22
+print(name)
+print(age)
+print(location)
